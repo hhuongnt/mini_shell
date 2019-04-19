bfb1fdc728ec7149ca69bc3c1309af72e2e40954
nhuong <n16dcat026@student.ptithcm.edu.vn>
2019-04-19 21:49:07 +0700

    *

--- path_expansions.py
+++ path_expansions.py
@@ -22,12 +22,12 @@ def handle_tilde_expansion(dest_path):
 			dest_path = dest_path[1:]
 
 			# if destination path is '+'
-			if dest_path == '+':
+			if dest_path[0] == '+':
 				dest_path = getenv("PWD")
 				return dest_path
 
 			# if destination path is '-'
-			elif dest_path == '-':
+			elif dest_path[0] == '-':
 				dest_path = getenv("OLDPWD")
 				return dest_path
 
@@ -42,5 +42,67 @@ def handle_tilde_expansion(dest_path):
 	return dest_path
 
 
-def handle_parameter_expansion(dest_path):
-	pass
+def create_dict(command_line, dict):
+	if '=' in command_line:
+		for i in range(len(command_line)):
+			if command_line[i] == '=':
+				break
+		key = command_line[:i]
+		value = command_line[i+1:]
+		dict[key] = value
+		return dict
+	else:
+		pass
+
+
+def handle_parameter_expansion(dest_path, dict):
+	if dest_path[0] == '$':
+
+		# if expression is empty
+		if len(dest_path) == 1:
+			return '$'
+
+		# handle expression
+		else:
+
+			# handle braces '{}'
+			if dest_path[1] == '{':
+				var = ''
+
+				# trace the variable in the braces
+				for char in dest_path[2:]:
+					if char == '}':
+						break
+					var += char
+
+				# if variable is already set
+				if var in dict.keys():
+					return dict[var]
+
+				# if variable is not set
+				return ''
+
+			# without braces '{}'
+			else:
+				dest_path = dest_path[1:]
+
+				# if variable is already set
+				if dest_path in dict.keys():
+					return dict[dest_path]
+
+				# if variable is not set
+				else:
+					return ''
+	else:
+		return ''
+
+
+def main():
+	dict = {}
+	while 1:
+		command_line = input()
+		if command_line[0] != '$':
+			dict = create_dict(command_line, dict)
+		else:
+			print(handle_parameter_expansion(command_line, dict))
+main()
